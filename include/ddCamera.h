c4cf36282d84b684eec44975073c367c93eff0d1
Moses <maadeagbo@gmail.com>
2018-02-14 05:37:26 -0600

    Testing ddCam to change the way lua interfaces w/ functions

--- include/ddCamera.h
+++ include/ddCamera.h
@@ -6,6 +6,7 @@
 */
 
 #include "ddIncludes.h"
+#include "LuaHooks.h"
 
 /**
  * \brief Struct for 6-sided frustum
@@ -16,79 +17,6 @@ struct FrustumBox {
   glm::vec3 corners[8];
   float d[6];
 };
-
-/**
-        Camera class implements FPS-style camera with quaternion rotations. Can
-        handle VR applications as well.
-*/
-// class ddCamera {
-//  public:
-//   ddCamera(const GLfloat screenW = 0.0f, const GLfloat screenH = 0.0f,
-//             const GLfloat fovh = 45.0f, const GLfloat nearP = 1.0f,
-//             const GLfloat farP = 10000.0f)
-//       : scr_width(screenW),
-//         scr_height(screenH),
-//         fov_h(glm::radians(fovh)),
-//         near_plane(nearP),
-//         far_plane(farP),
-//         active(false),
-//         vr_cam(false),
-//         vr_window(false),
-//         world_up(glm::vec4(0.0f, 1.0f, 0.0f, 1.0f)),
-//         cam_pos(glm::vec4(0.0f, 0.0f, 0.0f, 1.0f)),
-//         cam_quat(glm::quat()),
-//         cam_front(glm::vec3(0.0f, 0.0f, -1.0f)),
-//         cam_up(glm::vec3(0.0f, 1.0f, 0.0f)),
-//         _flag_parent(false) {}
-
-//   inline glm::vec4 pos() const { return cam_pos; }
-//   inline glm::quat camQuat() const { return cam_quat; }
-//   inline glm::vec4 worldUp() const { return world_up; }
-//   inline glm::vec3 front() const { return cam_front; }
-//   inline FrustumBox frustum() const { return cam_frustum; }
-//   void updateCamera(const glm::vec4 pos, const glm::quat quaternion);
-//   void SetupFrustum();
-//   inline void SetParent(const char* parentID) {
-//     _parent_id = parentID;
-//     _parent_idx = -1;
-//     _flag_parent = true;
-//   }
-//   inline void unParent() {
-//     _parent_id = "";
-//     _parent_idx = -1;
-//     _flag_parent = false;
-//   }
-//   inline void SetParentIndex(const int index) { _parent_idx = index; }
-//   inline int parentIndex() const { return _parent_idx; }
-//   inline std::string parentID() const { return _parent_id; }
-//   inline bool isChild() const { return _flag_parent; }
-
-//   std::string m_ID;
-//   glm::mat4 parent_transform;
-//   glm::vec3 vr_scr_pos;
-//   glm::vec3 vr_eye_pos;
-//   GLfloat scr_width;
-//   GLfloat scr_height;
-//   GLfloat fov_h;
-//   GLfloat near_plane;
-//   GLfloat far_plane;
-//   GLfloat cam_eye_dist;
-//   bool active;
-//   bool vr_cam;
-//   bool vr_window;
-
-//  private:
-//   std::string _parent_id;
-//   FrustumBox cam_frustum;
-//   glm::vec4 world_up;
-//   glm::vec4 cam_pos;
-//   glm::quat cam_quat;
-//   glm::vec3 cam_front;
-//   glm::vec3 cam_up;
-//   glm::vec3 cam_right;
-//   int _parent_idx;
-//   GLboolean _flag_parent;
-// };
 
 // namespace CamSpace {
 // glm::mat4 GetViewMatrix(const ddCamera* cam);
@@ -152,3 +80,7 @@ struct ddCam {
    */
   bool vr_flag = false;
 };
+
+
+const char* ddCam_meta_t_name();
+void log_meta_t_ddCam(lua_State *L);
--- src/ddCamera.cpp
+++ src/ddCamera.cpp
@@ -1,118 +1,10 @@
 #include "ddCamera.h"
 
-/**
-        Update camera internals (frustum, position, quaternion)
-        \param pos Vector-4 position to set camera to
-        \param quaternion Rotation to set camera to
-*/
-// void DD_Camera::updateCamera(const glm::vec4 pos, const glm::quat quaternion)
-// {
-//   cam_pos = pos;
-//   cam_quat = quaternion;
-//   SetupFrustum();
-// }
-
-/**
-        Calculate new camera frustum for current frame
-*/
-// void DD_Camera::SetupFrustum() {
-//   // recalculate front, right, and up for frustum calculation
-//   glm::vec3 front = glm::normalize(cam_quat * cam_front);
-//   glm::vec3 right = glm::normalize(glm::cross(front, glm::vec3(world_up)));
-//   glm::vec3 up = glm::cross(right, front);
-
-//   // calculate new frustum
-//   glm::vec3 nearCenter = glm::vec3(glm::vec3(cam_pos) + front * near_plane);
-//   glm::vec3 farCenter = glm::vec3(glm::vec3(cam_pos) + front * far_plane);
-//   GLfloat tang = tan(fov_h / 2);
-//   GLfloat ratio = (scr_height / scr_width);
-//   GLfloat wNear = tang * near_plane * 2.0f;
-//   GLfloat hNear = wNear * ratio;
-//   GLfloat wFar = tang * far_plane * 2.0f;
-//   GLfloat hFar = wFar * ratio;
-
-//   // corners
-//   cam_frustum.corners[0] = nearCenter + (up * hNear) + (right * wNear);
-//   cam_frustum.corners[1] = nearCenter - (up * hNear) + (right * wNear);
-//   cam_frustum.corners[2] = nearCenter + (up * hNear) - (right * wNear);
-//   cam_frustum.corners[3] = nearCenter - (up * hNear) - (right * wNear);
-//   cam_frustum.corners[4] = farCenter + (up * hFar) + (right * wFar);
-//   cam_frustum.corners[5] = farCenter - (up * hFar) + (right * wFar);
-//   cam_frustum.corners[6] = farCenter + (up * hFar) - (right * wFar);
-//   cam_frustum.corners[7] = farCenter - (up * hFar) - (right * wFar);
-
-//   // frustum top
-//   glm::vec3 topPos = nearCenter + (up * hNear);
-//   glm::vec3 tempVec = glm::normalize(topPos - glm::vec3(cam_pos));
-//   glm::vec3 upNorm = glm::cross(tempVec, right);
-
-//   // frustum bottom
-//   glm::vec3 botPos = nearCenter - (up * hNear);
-//   tempVec = glm::normalize(botPos - glm::vec3(cam_pos));
-//   glm::vec3 downNorm = glm::cross(right, tempVec);
-
-//   // frustum left
-//   glm::vec3 leftPos = nearCenter - (right * wNear);
-//   tempVec = glm::normalize(leftPos - glm::vec3(cam_pos));
-//   glm::vec3 leftNorm = glm::cross(tempVec, up);
-
-//   // frustum right normal ---> (nc + left * Wnear / 2) - p
-//   glm::vec3 rightPos = nearCenter + (right * wNear);
-//   tempVec = glm::normalize(rightPos - glm::vec3(cam_pos));
-//   glm::vec3 rightNorm = glm::cross(up, tempVec);
-
-//   // points
-//   cam_frustum.points[0] = nearCenter;
-//   cam_frustum.points[1] = farCenter;
-//   cam_frustum.points[2] = rightPos;
-//   cam_frustum.points[3] = leftPos;
-//   cam_frustum.points[4] = topPos;
-//   cam_frustum.points[5] = botPos;
-
-//   // normals
-//   cam_frustum.normals[0] = front;
-//   cam_frustum.normals[1] = -front;
-//   cam_frustum.normals[2] = rightNorm;
-//   cam_frustum.normals[3] = leftNorm;
-//   cam_frustum.normals[4] = upNorm;
-//   cam_frustum.normals[5] = downNorm;
-
-//   // D
-//   cam_frustum.d[0] = -glm::dot(cam_frustum.normals[0],
-//   cam_frustum.points[0]);
-//   cam_frustum.d[1] = -glm::dot(cam_frustum.normals[1],
-//   cam_frustum.points[1]);
-//   cam_frustum.d[2] = -glm::dot(cam_frustum.normals[2],
-//   cam_frustum.points[2]);
-//   cam_frustum.d[3] = -glm::dot(cam_frustum.normals[3],
-//   cam_frustum.points[3]);
-//   cam_frustum.d[4] = -glm::dot(cam_frustum.normals[4],
-//   cam_frustum.points[4]);
-//   cam_frustum.d[5] = -glm::dot(cam_frustum.normals[5],
-//   cam_frustum.points[5]);
-// }
-
 /*
-        Calculate and return view matrix
-*/
-// glm::mat4 CamSpace::GetViewMatrix(const DD_Camera* cam) {
-//   const glm::vec3 pos = glm::vec3(cam->pos());
-
-//   glm::vec3 front = glm::normalize(cam->camQuat() * cam->front());
-//   // cam->setFVec(glm::vec4(front.x, front.y, front.z, 1.f));
-
-//   glm::vec3 right =
-//       glm::normalize(glm::cross(front, glm::vec3(cam->worldUp())));
-//   glm::vec3 up = glm::normalize(glm::cross(right, front));
-
-//   return glm::lookAt(pos, pos + front, up);
-// }
-
-/*
-        Calculate and return view matrix for specified eye position
-        \param _left flag for calculating view from left eye
-        \param _right flag for calculating view from right eye
-        \param dist Distance between eye in meters
+	Calculate and return view matrix for specified eye position
+	\param _left flag for calculating view from left eye
+	\param _right flag for calculating view from right eye
+	\param dist Distance between eye in meters
 */
 // glm::mat4 CamSpace::GetViewMatrixVREye(const DD_Camera* cam, const bool
 // _left,
@@ -138,19 +30,11 @@
 // }
 
 /*
-        Calculate and return perspective projection matrix
-*/
-// glm::mat4 CamSpace::GetPerspecProjMatrix(const DD_Camera* cam) {
-//   return glm::perspectiveFov(cam->fov_h, cam->scr_width, cam->scr_height,
-//                              cam->near_plane, cam->far_plane);
-// }
-
-/*
-        Calculate and return off-axis perspective projection matrix
-        \param screenPos Absolute location of screen/monitor
-        \param scrWidth Horizontal length of screen
-        \param scrHeight Vertical length of screen
-        \param eyePos Absolute location of eye
+	Calculate and return off-axis perspective projection matrix
+	\param screenPos Absolute location of screen/monitor
+	\param scrWidth Horizontal length of screen
+	\param scrHeight Vertical length of screen
+	\param eyePos Absolute location of eye
 */
 // glm::mat4 CamSpace::GetOffAxisProjMatrix(const DD_Camera* cam,
 //                                          const glm::vec3 screenPos,
@@ -188,25 +72,14 @@
 //   return frst;
 // }
 
-/*
-        \todo remove all references
-*/
-// void CamSpace::PrintInfo(const DD_Camera& cam) {
-//   printf("\nCamera ID: %s\n", cam.m_ID.c_str());
+namespace {
+	const char* meta_name = "LuaClass.ddCam";
+}
 
-//   glm::vec4 pos = cam.pos();
-//   printf("\tPosition: %.3f, %.3f, %.3f\n", pos.x, pos.y, pos.z);
-//   glm::vec4 up = cam.worldUp();
-//   printf("\tUp: %.3f, %.3f, %.3f\n", up.x, up.y, up.z);
-//   glm::quat q = cam.camQuat();
+const char* ddCam_meta_t_name() {
+	return meta_name;
+}
 
-//   printf("\tRotation (quat): %.3f, %.3f, %.3f, %.3f\n", q.x, q.y, q.z, q.w);
-//   printf("\tFOV horizontal: %.1f degrees\n", glm::degrees(cam.fov_h));
-//   printf("\tNear plane: %.3f\n", cam.near_plane);
-//   printf("\tFar plane: %.3f\n", cam.far_plane);
-//   if (cam.active) {
-//     printf("\tActive: True\n");
-//   } else {
-//     printf("\tActive: False\n");
-//   }
-// }
+void log_metatable_ddCam(lua_State *L) {
+	
+}
